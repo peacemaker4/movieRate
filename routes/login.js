63de8c9cf2683042266430fb7cb395dc8ae10645
peacemaker4 <esmagametov.bek@gmail.com>
2022-05-27 15:42:00 +0600

    bcrypt + google OAuth 2.0

---
+++ routes/login.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const path = require('path');
+router
+    .route("/")
+    .get((req, res) => res.render(path.resolve("views/login.ejs")));
+module.exports = router;
